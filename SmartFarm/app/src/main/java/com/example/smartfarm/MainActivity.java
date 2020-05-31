a8a3e92d43b3c4e2bffaf7ac4f502149f23503d2
wenxiy <193367189@qq.com>
2020-05-31 11:32:17 +0800

    123

--- SmartFarm/app/src/main/java/com/example/smartfarm/MainActivity.java
+++ SmartFarm/app/src/main/java/com/example/smartfarm/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.smartfarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.TableLayout;
 
 public class MainActivity extends AppCompatActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.firstpage);
+        viewPager=findViewById(R.id.viewpager);
+        tabLayout=findViewById(R.id.table);
       //  customVideoView=findViewById(R.id.VideoView);
         //customVideoView.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.drawable.));
     }
-    private CustoVideoView customVideoView;
+//    private CustoVideoView customVideoView;
 }
